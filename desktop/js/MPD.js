867735f31abaf5a7dc113af53df69ec86fad3d57
bernard-dandrea <60914791+bernard-dandrea@users.noreply.github.com>
2026-02-01 18:29:46 +0100

    Add files via upload

--- desktop/js/MPD.js
+++ desktop/js/MPD.js
@@ -14,50 +14,54 @@
 * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 */
 
+// code repris de core/core/js/plugin.template.js/addCmdToTableDefault version 4.5.2
+// Modifications avec commentaire // BD
 
-
-
-/* Permet la réorganisation des commandes dans l'équipement */
-$("#table_cmd").sortable({
-    axis: "y",
-    cursor: "move",
-    items: ".cmd",
-    placeholder: "ui-state-highlight",
-    tolerance: "intersect",
-    forcePlaceholderSize: true
-})
-
-$("#table_cmd").delegate(".listEquipementInfo", 'click', function () {
-    var el = $(this)
-    jeedom.cmd.getSelectModal({ cmd: { type: 'info' } }, function (result) {
-        var calcul = el.closest('tr').find('.cmdAttr[data-l1key=configuration][data-l2key=' + el.data('input') + ']')
-        calcul.atCaret('insert', result.human)
-    })
-})
-
-$("#table_cmd").delegate(".listEquipementAction", 'click', function () {
-    var el = $(this)
-    var subtype = $(this).closest('.cmd').find('.cmdAttr[data-l1key=subType]').value()
-    jeedom.cmd.getSelectModal({ cmd: { type: 'action', subType: subtype } }, function (result) {
-        var calcul = el.closest('tr').find('.cmdAttr[data-l1key=configuration][data-l2key=' + el.attr('data-input') + ']')
-        calcul.atCaret('insert', result.human);
-    })
-})
-
-/* Fonction permettant l'affichage des commandes dans l'équipement */
 function addCmdToTable(_cmd) {
 
+    if (document.getElementById('table_cmd') == null) return
+    if (document.querySelector('#table_cmd thead') == null) {
+        table = '<thead>'
+        table += '<tr>'
+        table += '<th>{{Id}}</th>'
+        table += '<th>{{Nom}}</th>'
+        table += '<th>{{Type}}</th>'
+        table += '<th>{{Commande}}</th>'
+        // BD     table += '<th>{{Options}}</th>'
+        table += '<th>{{Paramètres}}</th>'
+        table += '<th>{{Valeur}}</th>'
+        table += '<th>{{Action}}</th>'
+        table += '</tr>'
+        table += '</thead>'
+        table += '<tbody>'
+        table += '</tbody>'
+        document.getElementById('table_cmd').insertAdjacentHTML('beforeend', table)
+    }
     if (!isset(_cmd)) {
         var _cmd = { configuration: {} }
     }
     if (!isset(_cmd.configuration)) {
         _cmd.configuration = {}
     }
-    var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">'
-    tr += '<td class="hidden-xs">'
+    var tr = '<tr>'
+    tr += '<td style="min-width:50px;width:70px;">'
     tr += '<span class="cmdAttr" data-l1key="id"></span>'
     tr += '</td>'
     tr += '<td>'
+    /*   BD code remplacé par les définitions suivantes
+    tr += '<div class="row">'
+    tr += '<div class="col-sm-6">'
+    tr += '<a class="cmdAction btn btn-default btn-sm" data-l1key="chooseIcon"><i class="fa fa-flag"></i> Icône</a>'
+    tr += '<span class="cmdAttr" data-l1key="display" data-l2key="icon" style="margin-left : 10px;"></span>'
+    tr += '</div>'
+    tr += '<div class="col-sm-6">'
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="name">'
+    tr += '</div>'
+    tr += '</div>'
+    tr += '<select class="cmdAttr form-control input-sm" data-l1key="value" style="display : none;margin-top : 5px;" title="{{La valeur de la commande vaut par défaut la commande}}">'
+    tr += '<option value="">Aucune</option>'
+    tr += '</select>'
+    */
     tr += '<div class="input-group">'
     tr += '<input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">'
     tr += '<span class="input-group-btn"><a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a></span>'
@@ -66,53 +70,61 @@ function addCmdToTable(_cmd) {
     tr += '<select class="cmdAttr form-control input-sm" data-l1key="value" style="display:none;margin-top:5px;" title="{{Commande info liée}}">'
     tr += '<option value="">{{Aucune}}</option>'
     tr += '</select>'
+    // BD fin des modifs
     tr += '</td>'
-    tr += '<td>';
-    tr += '<input class="cmdAttr form-control input-sm " data-l1key="logicalId" placeholder="{{logicalID}}">'
-    tr += '</td>';
     tr += '<td>'
     tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>'
     tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>'
     tr += '</td>'
-    tr += '<td>'
-    tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label> '
-    tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized" checked/>{{Historiser}}</label> '
-    tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label> '
-
-    tr += '<div style="margin-top:7px;">'
-    tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
-    tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
-    tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="unite" placeholder="Unité" title="{{Unité}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
-    tr += '</div>'
+    tr += '<td style="min-width:400px"><input class="cmdAttr form-control input-sm" data-l1key="logicalId" value="0" style="width : 70%; display : inline-block;" placeholder="{{Commande}}"><br/>'
     tr += '</td>'
-
-
-    tr += '<td>';
-    tr += '<span class="cmdAttr" data-l1key="htmlstate"></span>';
-    tr += '</td>';
+    // Bd tr += '<td>'
+    // Bd tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="returnStateValue" placeholder="{{Valeur retour d\'état}}" style="width:48%;display:inline-block;">'
+    // Bd tr += '<input class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="returnStateTime" placeholder="{{Durée avant retour d\'état (min)}}" style="width:48%;display:inline-block;margin-left:2px;">'
+    // Bd tr += '<select class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="updateCmdId" style="display : none;" title="{{Commande d\'information à mettre à jour}}">'
+    // BD tr += '<option value="">Aucune</option>'
+    // Bd tr += '</select>'
+    // Bd tr += '</td>'
+    tr += '<td>'
+    // BD tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" style="width:30%;display:inline-block;">'
+    // BD tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" style="width:30%;display:inline-block;">'
+    // BD tr += '<input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="{{Unité}}" title="{{Unité}}" style="width:30%;display:inline-block;margin-left:2px;">'
+    // BD tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="listValue" placeholder="{{Liste de valeur|texte séparé par ;}}" title="{{Liste}}">'
+    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> '
+    tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span> '
+    // Bd  tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label></span> '
+    tr += '</td>'
+    tr += '<td>'
+    tr += '<span class="cmdAttr" data-l1key="htmlstate"></span>'
+    tr += '</td>'
     tr += '<td>'
     if (is_numeric(_cmd.id)) {
         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> '
-        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>'
+        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>'
     }
-    tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove" title="{{Supprimer la commande}}"></i></td>'
+    tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>'
+    tr += '</td>'
     tr += '</tr>'
-    $('#table_cmd tbody').append(tr)
-    var tr = $('#table_cmd tbody tr').last()
+
+    let newRow = document.createElement('tr')
+    newRow.innerHTML = tr
+    newRow.addClass('cmd')
+    newRow.setAttribute('data-cmd_id', init(_cmd.id))
+    document.getElementById('table_cmd').querySelector('tbody').appendChild(newRow)
+
     jeedom.eqLogic.buildSelectCmd({
-        id: $('.eqLogicAttr[data-l1key=id]').value(),
+        id: document.querySelector('.eqLogicAttr[data-l1key="id"]').jeeValue(),
         filter: { type: 'info' },
         error: function (error) {
-            $('#div_alert').showAlert({ message: error.message, level: 'danger' })
+            jeedomUtils.showAlert({ message: error.message, level: 'danger' })
         },
         success: function (result) {
-            tr.find('.cmdAttr[data-l1key=value]').append(result)
-            tr.setValues(_cmd, '.cmdAttr')
-            jeedom.cmd.changeType(tr, init(_cmd.subType))
+            newRow.querySelector('.cmdAttr[data-l1key="value"]').insertAdjacentHTML('beforeend', result)
+            newRow.setJeeValues(_cmd, '.cmdAttr')
+            jeedom.cmd.changeType(newRow, init(_cmd.subType))
         }
     })
 }
-
 
 function printEqLogic(_eqLogic) {
 
@@ -120,63 +132,60 @@ function printEqLogic(_eqLogic) {
 }
 
 
-$('#bt_TestConnexionMPD').on('click', function () {
-    $.ajax({// fonction permettant de faire de l'ajax
-        type: "POST", // methode de transmission des données au fichier php
-        url: "plugins/MPD/core/ajax/MPD.ajax.php", // url du fichier php
-        // LA FONCTION test_connexion DOIT ETRE DEFINIE DANS LE FICHIER CI-DESSUS
+document.querySelector('#bt_TestConnexionMPD').addEventListener('click', function () {
+
+    const eqLogicId = document.querySelector('.eqLogicAttr[data-l1key="id"]').value;
+
+    var paramsAJAX = {
+        url: 'plugins/MPD/core/ajax/MPD.ajax.php',
         data: {
-            action: "test_connexion",
-            id: $('.eqLogicAttr[data-l1key=id]').value(),
+            action: 'test_connexion',
+            id: eqLogicId
         },
+        type: "POST",
         dataType: 'json',
-        error: function (request, status, error) {
-            handleAjaxError(request, status, $('#div_DetectBin'));
+        success: function (data) {
+            jeedomUtils.showAlert({ message: data.result, level: 'success' });
         },
-        success: function (data) { // si l'appel a bien fonctionné
-            if (data.state != 'ok') {
-                $('#div_alert').showAlert({ message: data.result, level: 'danger' });
-                return;
-            }
-            // window.location.reload();  // si on recharge la fenetre, on perd le message envoyé par test_connexion
+        error: function (request, status, error) {
+            handleAjaxError(request, status, error);
         }
-    });
+    };
+    domUtils.ajax(paramsAJAX);
+
 });
 
+document.querySelector('#bt_Generer_Commandes').addEventListener('click', function () {
 
-$('#bt_Generer_Commandes').on('click', function () {
-    $.ajax({// fonction permettant de faire de l'ajax
-        type: "POST", // methode de transmission des données au fichier php
-        url: "plugins/MPD/core/ajax/MPD.ajax.php", // url du fichier php
-        // LA FONCTION bt_Generer Commandes DOIT ETRE DEFINIE DANS LE FICHIER CI-DESSUS
+    const eqLogicId = document.querySelector('.eqLogicAttr[data-l1key="id"]').value;
+
+    var paramsAJAX = {
+        url: 'plugins/MPD/core/ajax/MPD.ajax.php',
         data: {
-            action: "generer_commandes",
-            id: $('.eqLogicAttr[data-l1key=id]').value(),
+            action: 'generer_commandes',
+            id: eqLogicId
         },
+        type: "POST",
         dataType: 'json',
-        error: function (request, status, error) {
-            handleAjaxError(request, status, $('#div_DetectBin'));
+        success: function (data) {
+            //  location.reload();
+
         },
-        success: function (data) { // si l'appel a bien fonctionné
-            if (data.state != 'ok') {
-                $('#div_alert').showAlert({ message: data.result, level: 'danger' });
-                return;
-            }
-            window.location.reload();  // si on recharge la fenetre, on perd le message envoyé par test_connexion
+        error: function (request, status, error) {
+            handleAjaxError(request, status, error);
         }
-    });
+    };
+    domUtils.ajax(paramsAJAX);
+
 });
 
-$("#bt_create_command").on('click', function (event) {
-    addCmdToTable({ type: 'action' })
-    modifyWithoutSave = true
-  })
-  
+document.querySelector('#bt_create_command').addEventListener('click', function (event) {
 
-$("#table_cmd").sortable({ axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true });
+    addCmdToTable({ type: 'action' });
 
-
-
+    // Modification de la variable globale de suivi de modification
+    modifyWithoutSave = true;
+});
 
 
 
