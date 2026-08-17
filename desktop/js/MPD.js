76577409e3a420b902e4b7f67ce90350ea3251bd
bernard-dandrea <60914791+bernard-dandrea@users.noreply.github.com>
2026-08-17 18:50:42 +0200

    Add files via upload

--- desktop/js/MPD.js
+++ desktop/js/MPD.js
@@ -1,7 +1,7 @@
 /* This file is part of Jeedom.
 *
 
-// Last Modified : 2026/08/16 11:32:04
+// Last Modified : 2026/08/17 18:21:49
 
 * Jeedom is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -179,6 +179,42 @@ document.querySelector('#bt_Generer_Commandes').addEventListener('click', functi
     domUtils.ajax(paramsAJAX);
 
 });
+
+document.querySelector('#bt_set_layout').addEventListener('click', function () {
+
+    var eqLogicId = document.querySelector('.eqLogicAttr[data-l1key="id"]').jeeValue();
+
+    var paramsAJAX = {
+        type: "POST",
+        url: 'plugins/MPD/core/ajax/MPD.ajax.php',
+        data: {
+            action: 'bt_set_layout',
+            id: eqLogicId
+        },
+        dataType: 'json',
+        error: function (request, status, error) {
+            handleAjaxError(request, status, error)
+        },
+        success: function (data) {
+            var message = data.result;
+
+            var level = 'success';
+            if (message.substr(0, 2) === 'KO') {
+                level = 'info';
+            }
+            if (message.length >= 4) {
+                message = message.substr(3);
+            }
+            jeedomUtils.showAlert({
+                message: message,
+                level: level
+            })
+        }
+    }
+    domUtils.ajax(paramsAJAX);
+
+});
+
 
 document.querySelector('#bt_create_command').addEventListener('click', function (event) {
 
